9103478913e4226d31b5427d51679e3f72d6d34b
Yahya Al fayad <yahya.alfayad@plotprojects.com>
2022-11-30 14:32:37 +0100

    Small code cleanup
    Updated PlotProjects SDK version and small code cleanup

--- App.js
+++ App.js
@@ -6,19 +6,19 @@
  * @flow strict-local
  */
 
-import React from 'react';
+import React from "react";
 import {
   SafeAreaView,
   StyleSheet,
   ScrollView,
   View,
   Text,
-  StatusBar, 
-  Button, 
+  StatusBar,
+  Button,
   Platform,
-  NativeModules, 
-  PermissionsAndroid
-} from 'react-native';
+  NativeModules,
+  PermissionsAndroid,
+} from "react-native";
 
 import {
   Header,
@@ -26,88 +26,102 @@ import {
   Colors,
   DebugInstructions,
   ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+} from "react-native/Libraries/NewAppScreen";
 
-import Plot from 'plotprojects-react-native-module';
+import Plot from "plotprojects-react-native-module";
 
 const initializePlot = async () => {
+  Plot.initialize();
 
-    Plot.initialize();
+  Plot.setAdvertisingId("jdlsfksdkjf", true);
 
-    Plot.setAdvertisingId('jdlsfksdkjf', true);
+  Plot.sendAttributionEvent("read_flyer", "discounts_week_12");
+  Plot.setStringSegmentationProperty("gender", "man");
 
-    Plot.sendAttributionEvent("read_flyer", "discounts_week_12");
-    Plot.setStringSegmentationProperty("gender", "man");
+  console.debug("Registering notificaiton filter");
 
-    console.debug('Registering notificaiton filter');
-    
-    Plot.registerNotificationFilter((batchId, notifications) => {
-        console.debug('Received notifications in the filter with batchId: ' + batchId);
-        notifications.forEach((n) => {
-          n['message'] = n['message'] + ' after filtering';
-        });
-		console.debug(JSON.stringify(notifications));
-        Plot.filterNotifications(batchId, JSON.stringify(notifications));
+  Plot.registerNotificationFilter((batchId, notifications) => {
+    console.debug(
+      "Received notifications in the filter with batchId: " + batchId
+    );
+    notifications.forEach((n) => {
+      n["message"] = n["message"] + " after filtering";
     });
-	
-	console.debug('Registering geotrigger handler');
-	
-	Plot.registerGeotriggerHandler((batchId, geotriggers) => {
-		console.debug('Received geotriggers in the handler with batchId: ' + batchId);
-		console.debug(JSON.stringify(geotriggers));
-		Plot.handleGeotriggers(batchId, JSON.stringify(geotriggers));
-	});
-	
-	console.debug('Registering notificaiton open handler');
-	
-	Plot.registerNotificationOpenHandler((openedNotification) => {
-		console.debug('Received opened notifications in the handler');
-		console.debug(JSON.stringify(openedNotification));
-	});
-}
+    console.debug(JSON.stringify(notifications));
+    Plot.filterNotifications(batchId, JSON.stringify(notifications));
+  });
+
+  console.debug("Registering geotrigger handler");
+
+  Plot.registerGeotriggerHandler((batchId, geotriggers) => {
+    console.debug(
+      "Received geotriggers in the handler with batchId: " + batchId
+    );
+    console.debug(JSON.stringify(geotriggers));
+    Plot.handleGeotriggers(batchId, JSON.stringify(geotriggers));
+  });
+
+  console.debug("Registering notificaiton open handler");
+
+  Plot.registerNotificationOpenHandler((openedNotification) => {
+    console.debug("Received opened notifications in the handler");
+    console.debug(JSON.stringify(openedNotification));
+  });
+};
 
 const requestLocationPermission = async () => {
-    try {
-      if(Platform.OS === "ios") {
+  try {
+    if (Platform.OS === "ios") {
+      initializePlot();
+    } else {
+      const granted = await PermissionsAndroid.requestMultiple([
+        "android.permission.POST_NOTIFICATIONS",
+        PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
+        PermissionsAndroid.PERMISSIONS.ACTIVITY_RECOGNITION,
+      ]);
+      if (
+        granted["android.permission.ACCESS_COARSE_LOCATION"] ===
+          PermissionsAndroid.RESULTS.GRANTED ||
+        granted["android.permission.ACCESS_FINE_LOCATION"] ===
+          PermissionsAndroid.RESULTS.GRANTED
+      ) {
+        console.debug("Foreground location permission granted!");
+        const granted = await PermissionsAndroid.request(
+          PermissionsAndroid.PERMISSIONS.ACCESS_BACKGROUND_LOCATION
+        );
+        if (
+          granted["android.permission.ACCESS_BACKGROUND_LOCATION"] ===
+          PermissionsAndroid.RESULTS.GRANTED
+        ) {
+          console.debug("Background location permission granted!");
+        } else {
+          console.debug("Background location permission denied!");
+        }
         initializePlot();
       } else {
-        const granted = await PermissionsAndroid.requestMultiple(
-          [
-			  'android.permission.POST_NOTIFICATIONS',
-			  PermissionsAndroid.PERMISSIONS.ACCESS_COARSE_LOCATION,
-			  PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-  			  PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
-			  PermissionsAndroid.PERMISSIONS.ACTIVITY_RECOGNITION]);
-        if (granted['android.permission.ACCESS_COARSE_LOCATION'] === PermissionsAndroid.RESULTS.GRANTED
-              || granted['android.permission.ACCESS_FINE_LOCATION'] === PermissionsAndroid.RESULTS.GRANTED
-        ) {
-			console.debug("Foreground location permission granted!");
-			const granted = await PermissionsAndroid.request(PermissionsAndroid.PERMISSIONS.ACCESS_BACKGROUND_LOCATION);
-			if (granted['android.permission.ACCESS_BACKGROUND_LOCATION'] === PermissionsAndroid.RESULTS.GRANTED) {
-				console.debug("Background location permission granted!");
-			} else {
-				console.debug("Background location permission denied!");
-			}
-          initializePlot();
-        } else {
-          console.debug("Location permission denied!. Not initializing PlotProjects SDK.");
-        }
+        console.debug(
+          "Location permission denied!. Not initializing PlotProjects SDK."
+        );
       }
-    } catch (err) {
-      console.warn(err);
     }
-  };
+  } catch (err) {
+    console.warn(err);
+  }
+};
 
 requestLocationPermission();
 
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
+          style={styles.scrollView}
+        >
           <Header />
           {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
@@ -115,11 +129,11 @@ const App: () => React$Node = () => {
             </View>
           )}
           <View style={styles.body}>
-          <View style={styles.sectionContainer}>
-          <Button
-              title="Email debug log"
-              onPress={() => Plot.mailDebugLog()}
-            />
+            <View style={styles.sectionContainer}>
+              <Button
+                title="Email debug log"
+                onPress={() => Plot.mailDebugLog()}
+              />
             </View>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Step One</Text>
@@ -159,7 +173,7 @@ const styles = StyleSheet.create({
     backgroundColor: Colors.lighter,
   },
   engine: {
-    position: 'absolute',
+    position: "absolute",
     right: 0,
   },
   body: {
@@ -171,25 +185,25 @@ const styles = StyleSheet.create({
   },
   sectionTitle: {
     fontSize: 24,
-    fontWeight: '600',
+    fontWeight: "600",
     color: Colors.black,
   },
   sectionDescription: {
     marginTop: 8,
     fontSize: 18,
-    fontWeight: '400',
+    fontWeight: "400",
     color: Colors.dark,
   },
   highlight: {
-    fontWeight: '700',
+    fontWeight: "700",
   },
   footer: {
     color: Colors.dark,
     fontSize: 12,
-    fontWeight: '600',
+    fontWeight: "600",
     padding: 4,
     paddingRight: 12,
-    textAlign: 'right',
+    textAlign: "right",
   },
 });
 
